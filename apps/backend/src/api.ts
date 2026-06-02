1631ae70a21044505d0af76c9975ca392eac7248
Shashank Tripathi <caspersky@Shashanks-MacBook-Pro.local>
2026-06-02 16:28:41 +0530

    mongo fix

--- apps/backend/src/api.ts
+++ apps/backend/src/api.ts
@@ -2,13 +2,10 @@ import serverless from "serverless-http";
 import app from "./app";
 import { connectDB } from "./config/db";
 
-export default serverless(async (req:any, res:any) => {
+export default serverless(async (req, res) => {
   console.log("🔥 HANDLER CALLED");
-  console.log("🔥 CONNECTING TO MONGO");
 
   await connectDB();
-
-  console.log("🚀 REQUEST HANDLING STARTED");
 
   return app(req, res);
 });
