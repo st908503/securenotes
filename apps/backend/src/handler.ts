3448d5f17d78174ae2f62bb426963851b3f29e1e
Shashank Tripathi <caspersky@Shashanks-MacBook-Pro.local>
2026-06-02 15:12:46 +0530

    mongo fix

--- apps/backend/src/handler.ts
+++ apps/backend/src/handler.ts
@@ -1,8 +1,9 @@
 import serverless from "serverless-http";
 import app from "./app";
 import { connectDB } from "./config/db";
+import { Request, Response } from "express";
 
-export const handler = serverless(async (req, res) => {
-  await connectDB();   // MUST happen here
+export const handler = serverless(async (req: Request, res: Response) => {
+  await connectDB();
   return app(req, res);
 });
